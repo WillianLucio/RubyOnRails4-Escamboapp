0ad65d150794db2a9796463647c4cbac9a865d16
willian <willian.l.lucio@gmail.com>
2018-10-29 16:58:34 -0200

    Lesson 80 - Count ads with counter_cache.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029175533) do
+ActiveRecord::Schema.define(version: 20181029185104) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(version: 20181029175533) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "ads_count",   default: 0
   end
 
   create_table "members", force: :cascade do |t|
