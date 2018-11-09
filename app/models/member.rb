092b5407058d30f4db946852c01e1bc30bf7a63d
willian <willian.l.lucio@gmail.com>
2018-11-09 10:45:43 -0200

    Lesson 101 - Nested attributes with devise.

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,7 +6,8 @@ class Member < ActiveRecord::Base
   # Associations
   has_many :ads
   has_one :profile_member
-  
+  accepts_nested_attributes_for :profile_member
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
