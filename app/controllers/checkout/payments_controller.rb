8d09e5b8c6b5b6285e1ff67352087223b43f53d3
willian <willian.l.lucio@gmail.com>
2018-11-21 08:57:13 -0200

    Lesson 253 - Verifying information in the sandbox.

--- app/controllers/checkout/payments_controller.rb
+++ app/controllers/checkout/payments_controller.rb
@@ -9,3 +9,8 @@ class Checkout::PaymentsController < ApplicationController
     render text: "Processando...   Pedido: #{order.status_i18n} - Anúncio: #{ad.status_i18n}"
   end
 end
+
+# Email: c51768499980820202041@sandbox.pagseguro.com.br
+# Senha: p3U3VEDDYrwX1XYp
+# Número: 4111111111111111
+# Bandeira: VISA Válido até: 12/2030 CVV: 123
